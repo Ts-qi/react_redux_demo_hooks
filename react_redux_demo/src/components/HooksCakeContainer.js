71eefa71f01ea670061304366b410c7d4dd87062
qi.tang <qi.tang@weimob.com>
2020-06-05 20:15:15 +0800

    使用 hooks 并且增加 redux 中封装的 useSelector 和 useDispatch

---
+++ react_redux_demo/src/components/HooksCakeContainer.js
@@ -0,0 +1,16 @@
+import React  from 'react'
+ import { useSelector , useDispatch} from 'react-redux';
+ import { buyCake } from '../redux'
+
+function HooksCakeContainer (props){
+	const numOfCakes = useSelector( state => state.numOfCakes)
+	const dispatch = useDispatch()
+	return(
+		<div>
+			 <h2>num of cakes , { numOfCakes }</h2>
+			 <button onClick={()=> dispatch(buyCake())}>	Buy cakes</button>
+		</div>
+	)
+};
+
+export default HooksCakeContainer;
